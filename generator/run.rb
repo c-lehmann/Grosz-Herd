686ee00150777b9697b97810f179d29911d33719
cl@c-lehmann.de <clehmann@ozenfant.local>
2015-01-16 20:44:07 +0100

    Fitting Content-type for error-msgs.

--- generator/run.rb
+++ generator/run.rb
@@ -10,22 +10,26 @@ end
 
 get '/button/:year' do |year|
   begin
+    button = GroszHerd::Button.new year 
+    
     content_type "image/png"
     headers "Content-disposition" => "attachment; filename=grosz_herd_button_" + year +".png"
-    button = GroszHerd::Button.new year 
     button.image.to_blob
   rescue ArgumentError => e
+    content_type "text/plain"
     halt 400, {'Content-Type' => 'text/plain'}, e.message
   end
 end
 
 get '/sheet/:year' do |year|
   begin
+    sheet = GroszHerd::Sheet.new year 
+    
     content_type "image/png"
     headers "Content-disposition" => "attachment; filename=grosz_herd_buttons_" + year +".png"
-    sheet = GroszHerd::Sheet.new year 
     sheet.image.to_blob
   rescue ArgumentError => e
+    content_type "text/plain"
     halt 400, {'Content-Type' => 'text/plain'}, e.message
   end
 end
